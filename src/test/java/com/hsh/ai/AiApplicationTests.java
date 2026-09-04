96bc4f62dbfa152850440f582d37ac538653bc81
HseongH <hh4518@gmail.com>
2026-09-05 00:27:57 +0900

    first commit

---
+++ src/test/java/com/hsh/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsh.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
